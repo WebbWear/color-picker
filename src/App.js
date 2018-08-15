05a81275a1f1d48ce3e342a0d89d5525f1d82ed8
Jeffrey Webb <jeff@MacBook-Pro.localdomain>
2018-08-15 15:46:22 -0400

    sliders

--- src/App.js
+++ src/App.js
@@ -3,16 +3,80 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+
+  updateHue = (event) => {
+    const newHue = event.target.value
+    this.setState({
+      hue: newHue
+  })
+}
+
+  updateSaturation = (event) => {
+    const newSaturation = event.target.value
+    this.setState({
+      saturation: newSaturation
+    })    
+  }
+
+  updateLightness = (event) => {
+    const newLightness = event.target.value
+    this.setState({
+      lightness: newLightness
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Color Picker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="App-intro">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            // onInput={this.updateHue} 
+            onChange={this.updateHue}
+            value={this.state.hue}
+          />
+          <input
+            type="range"
+            min="0"
+            max="100"
+            // onInput={this.updateSaturation} 
+            onChange={this.updateSaturation}
+            value={this.state.saturation}
+          />
+          <input
+            type="range"
+            min="0"
+            max="100"
+            // onInput={this.updateLightness} 
+            onChange={this.updateLightness}
+            value={this.state.lightness}
+          />
+        </section>
+
+        <div
+          className="shape"
+          style={
+            {
+              height: `${this.state.height}em`,
+              width: `${this.state.width}em`
+            }
+          }>
+        </div>
       </div>
     );
   }
